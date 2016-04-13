db5b74d1647bac09c7d7f6ec669513a869ac0e54
John Calabrese <jcalabrese@pivotal.io>
2016-04-12 22:32:13 -0400

    adding some setters so we can eventually do some validation on the object's values

---
+++ deploymentmanifest.go
@@ -0,0 +1,54 @@
+package enaml
+
+func (s *DeploymentManifest) SetName(n string) (err error) {
+	s.Name = n
+	return
+}
+
+func (s *DeploymentManifest) AddRelease(r Release) (err error) {
+	s.Releases = append(s.Releases, r)
+	return
+}
+
+func (s *DeploymentManifest) AddNetwork(n DeploymentNetwork) (err error) {
+	s.Networks = append(s.Networks, n)
+	return
+}
+
+func (s *DeploymentManifest) AddResourcePool(r ResourcePool) (err error) {
+	s.ResourcePools = append(s.ResourcePools, r)
+	return
+}
+
+func (s *DeploymentManifest) AddDiskPool(d DiskPool) (err error) {
+	s.DiskPools = append(s.DiskPools, d)
+	return
+}
+
+func (s *DeploymentManifest) SetCompilation(c Compilation) (err error) {
+	s.Compilation = c
+	return
+}
+
+func (s *DeploymentManifest) SetUpdate(u Update) (err error) {
+	s.Update = u
+	return
+}
+
+func (s *DeploymentManifest) AddJob(j Job) (err error) {
+	s.Jobs = append(s.Jobs, j)
+	return
+}
+
+func (s *DeploymentManifest) AddProperty(k string, val interface{}) (err error) {
+	if s.Properties == nil {
+		s.Properties = make(map[string]interface{})
+	}
+	s.Properties[k] = val
+	return
+}
+
+func (s *DeploymentManifest) SetCloudProvider(c CloudProvider) (err error) {
+	s.CloudProvider = c
+	return
+}
---
+++ deploymentmanifest_test.go
@@ -0,0 +1,86 @@
+package enaml_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/xchapter7x/enaml"
+)
+
+var _ = Describe("DeploymentManifest", func() {
+	Describe("given base setters", func() {
+		Context("when called", func() {
+			var DefaultName = "testdeploy"
+			var controlDeploymentManifest = new(DeploymentManifest)
+			var testDeploymentManifest = new(DeploymentManifest)
+			BeforeEach(func() {
+				controlDeploymentManifest = &DeploymentManifest{
+					Name: DefaultName,
+					Releases: []Release{
+						NewFooRelease(DefaultName, DefaultName),
+					},
+					Networks: []DeploymentNetwork{
+						NewFooNetwork(DefaultName),
+					},
+					ResourcePools: []ResourcePool{
+						NewFooResource(DefaultName, DefaultName),
+					},
+					DiskPools: []DiskPool{
+						NewFooDiskPool("db", 10240),
+					},
+				}
+			})
+			It("then it should set the values in the given DeploymentManifest", func() {
+				testDeploymentManifest.SetName(DefaultName)
+				testDeploymentManifest.AddRelease(NewFooRelease(DefaultName, DefaultName))
+				testDeploymentManifest.AddNetwork(NewFooNetwork(DefaultName))
+				testDeploymentManifest.AddResourcePool(NewFooResource(DefaultName, DefaultName))
+				testDeploymentManifest.AddDiskPool(NewFooDiskPool("db", 10240))
+				Ω(*testDeploymentManifest).Should(Equal(*controlDeploymentManifest))
+			})
+		})
+	})
+})
+
+func NewFooDiskPool(name string, size int) DiskPool {
+	return DiskPool{
+		Name:     name,
+		DiskSize: size,
+	}
+}
+
+func NewFooNetwork(networkName string) DeploymentNetwork {
+	return ManualNetwork{
+		Name: networkName,
+		Type: "manual",
+		Subnets: []Subnet{
+			Subnet{
+				Range:   "10.0.0.0/24",
+				DNS:     "10.0.0.2",
+				Gateway: "10.0.0.1",
+				CloudProperties: CloudProperties{
+					"name": "NETWORK_NAME",
+				},
+			},
+		},
+	}
+}
+
+func NewFooRelease(version, sha string) Release {
+	return Release{
+		Name: "concourse",
+		URL:  "https://bosh.io/d/github.com/concourse/concourse?v=" + version,
+		SHA1: sha,
+	}
+}
+
+func NewFooResource(resourceName, networkName string) ResourcePool {
+	return ResourcePool{
+		Name:    resourceName,
+		Network: networkName,
+		CloudProperties: CloudProperties{
+			"cpu":  2,
+			"ram":  4096,
+			"disk": 10240,
+		},
+	}
+}
