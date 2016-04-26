b29bb1fff13bac297236c1a6b75d210f4e107fdf
Shawn Neal <sneal@sneal.net>
2016-04-26 13:36:28 -0700

    Remove uncessary withBoshRelease syntax

--- diff/boshreleasediffer.go
+++ diff/boshreleasediffer.go
@@ -17,54 +17,54 @@ type boshReleaseDiffer struct {
 
 func (d boshReleaseDiffer) Diff() (result Result, err error) {
 	result = Result{}
-	err = d.withBoshRelease(d.R1Path, func(r1 *boshRelease) error {
-		err = d.withBoshRelease(d.R2Path, func(r2 *boshRelease) error {
-			result.Deltas = pretty.Diff(r1, r2)
-			return nil
-		})
-		return nil
-	})
+	var r1, r2 *boshRelease
+	r1, err = d.loadBoshRelease(d.R1Path)
+	if err != nil {
+		return
+	}
+	r2, err = d.loadBoshRelease(d.R2Path)
+	if err != nil {
+		return
+	}
+	result.Deltas = pretty.Diff(r1, r2)
 	return
 }
 
 func (d boshReleaseDiffer) DiffJob(job string) (result Result, err error) {
 	result = Result{}
-	err = d.withBoshRelease(d.R1Path, func(r1 *boshRelease) error {
-		err = d.withBoshRelease(d.R2Path, func(r2 *boshRelease) error {
+	var r1, r2 *boshRelease
+	if r1, err = d.loadBoshRelease(d.R1Path); err == nil {
+		if r2, err = d.loadBoshRelease(d.R2Path); err == nil {
 			var (
 				job1, job2 enaml.JobManifest
 				ok         bool
 			)
 			if job1, ok = r1.JobManifests[job]; !ok {
-				return fmt.Errorf("Couldn't find job '%s' in release 1", job)
+				err = fmt.Errorf("Couldn't find job '%s' in release 1", job)
+				return
 			}
 			if job2, ok = r2.JobManifests[job]; !ok {
-				return fmt.Errorf("Couldn't find job '%s' in release 2", job)
+				err = fmt.Errorf("Couldn't find job '%s' in release 2", job)
+				return
 			}
 			result.Deltas = pretty.Diff(job1, job2)
-			return nil
-		})
-		return nil
-	})
+		}
+	}
 	return
 }
 
-type withBoshReleaseFunc func(r *boshRelease) error
-
-func (d boshReleaseDiffer) withBoshRelease(path string, fn withBoshReleaseFunc) (err error) {
+func (d boshReleaseDiffer) loadBoshRelease(path string) (release *boshRelease, err error) {
 	var rr io.ReadCloser
 	rr, err = d.ReleaseRepo.Read(path)
 	if err != nil {
-		return err
+		return
 	}
 	defer func() {
 		if cerr := rr.Close(); cerr != nil {
 			err = cerr
 		}
 	}()
-	release := newBoshRelease()
-	if err = release.readBoshRelease(rr); err != nil {
-		return err
-	}
-	return fn(release)
+	release = newBoshRelease()
+	err = release.readBoshRelease(rr)
+	return
 }
