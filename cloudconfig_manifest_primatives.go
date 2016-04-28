bd025a6736fe7ed3fae0994e8931387f5ba4d7be
John Calabrese <jcalabrese@pivotal.io>
2016-04-28 11:15:11 -0400

    [#118197197] adding helper setters to cloudconfigmanifest

--- cloudconfig_manifest_primatives.go
+++ cloudconfig_manifest_primatives.go
@@ -17,3 +17,23 @@ type AZ struct {
 	Name            string      `yaml:"name:omitempty"`
 	CloudProperties interface{} `yaml:"cloud_properties,omitempty"`
 }
+
+func (s *CloudConfigManifest) AddAZ(az AZ) (err error) {
+	s.AZs = append(s.AZs, az)
+	return
+}
+
+func (s *CloudConfigManifest) AddVMType(vmt VMType) (err error) {
+	s.VMTypes = append(s.VMTypes, vmt)
+	return
+}
+
+func (s *CloudConfigManifest) AddDiskType(dskt DiskPool) (err error) {
+	s.DiskTypes = append(s.DiskTypes, dskt)
+	return
+}
+
+func (s *CloudConfigManifest) AddNetwork(ntw DeploymentNetwork) (err error) {
+	s.Networks = append(s.Networks, ntw)
+	return
+}
