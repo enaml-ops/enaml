6386f7f5003ed598ea9ea99a360f263104ed176d
John Calabrese <jcalabrese@pivotal.io>
2016-04-28 12:32:28 -0400

    [#118197197] adding aws regions as constants

---
+++ cloudproperties/aws/const.go
@@ -0,0 +1,16 @@
+package awscloudproperties
+
+const (
+	//Regions - this is a list of available aws regions
+	APNortheast  = "ap-northeast-1"
+	APSoutheast  = "ap-southeast-1"
+	APSoutheast2 = "ap-southeast-2"
+	EUWest       = "eu-west-1"
+	EUCentral    = "eu-central-1"
+	USEast       = "us-east-1"
+	USWest       = "us-west-1"
+	USWest2      = "us-west-2"
+	SAEast       = "sa-east-1"
+	USGovWest    = "us-gov-west-1"
+	CNNorth      = "cn-north-1"
+)
