240a6cb5d0207a4d88ce902833996c62b3d83028
Shawn Neal <sneal@sneal.net>
2016-04-28 12:03:02 -0700

    Improve job output

--- cmd/enaml/show.go
+++ cmd/enaml/show.go
@@ -35,29 +35,30 @@ func (s *show) All(w io.Writer) error {
 }
 
 func (s *show) printBoshRelease(w io.Writer, br *release.BoshRelease) {
-	fmt.Fprintln(w, "------------------------------------------------------")
-	fmt.Fprintln(w, br.ReleaseManifest.Name)
-	fmt.Fprintln(w, "------------------------------------------------------")
 	for _, j := range br.JobManifests {
-		s.printBoshJob(w, j)
+		s.printBoshJob(w, j, br.ReleaseManifest.Name)
 	}
 	fmt.Fprintln(w)
 }
 
-func (s *show) printBoshJob(w io.Writer, j enaml.JobManifest) {
-	fmt.Fprintln(w, j.Name)
+func (s *show) printBoshJob(w io.Writer, j enaml.JobManifest, boshReleaseName string) {
+	fmt.Fprintln(w, "------------------------------------------------------")
+	fmt.Fprintln(w, fmt.Sprintf("Release: %s", boshReleaseName))
+	fmt.Fprintln(w, fmt.Sprintf("Job:     %s", j.Name))
+	fmt.Fprintln(w, "------------------------------------------------------")
 	for pname, p := range j.Properties {
-		fmt.Fprintln(w, fmt.Sprintf("  %s", pname))
+		fmt.Fprintln(w, pname)
 		s.printBoshJobProperty(w, p)
+		fmt.Fprintln(w)
 	}
 	fmt.Fprintln(w)
 }
 
 func (s *show) printBoshJobProperty(w io.Writer, p enaml.JobManifestProperty) {
 	if len(p.Description) > 0 {
-		fmt.Fprintln(w, fmt.Sprintf("    Description: %s", p.Description))
+		fmt.Fprintln(w, fmt.Sprintf("  Description: %s", p.Description))
 	}
 	if p.Default != nil {
-		fmt.Fprintln(w, fmt.Sprintf("    Default: %v", p.Default))
+		fmt.Fprintln(w, fmt.Sprintf("  Default: %v", p.Default))
 	}
 }
