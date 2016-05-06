dd0dda94428e14fc5a500461a96cbb4ea766645f
Shawn Neal <sneal@sneal.net>
2016-05-06 14:12:06 -0700

    Add new streamlined CLI commands
    - support: diff, show, generate commands only
    - use --job flag for filtering commands by job

--- cmd/enaml/main.go
+++ cmd/enaml/main.go
@@ -23,72 +23,86 @@ func init() {
 }
 
 const (
-	CacheDir  = ".cache"
+	// CacheDir is the location downloaded releases are stored
+	CacheDir = ".cache"
+	// OutputDir is where the generate command outputs generated structs
 	OutputDir = "./enaml-gen"
 )
 
 var (
-	Version   string
-	cacheDir  = CacheDir
-	outputDir = OutputDir
+	// Version is the enaml version
+	Version     string
+	cacheDir    = CacheDir
+	outputDir   = OutputDir
+	releaseRepo = pull.Release{CacheDir: cacheDir}
 )
 
 func main() {
+	var err error
 	app := cli.NewApp()
+	app.Name = "enaml"
+	app.Usage = "Because (EN)ough with the y(AML) already"
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name: "John Calabrese",
+		},
+		cli.Author{
+			Name: "Caleb Washburn",
+		},
+		cli.Author{
+			Name:  "Shawn Neal",
+			Email: "sneal@sneal.net",
+		},
+	}
 	app.Version = Version
 	app.Commands = []cli.Command{
 		{
-			Name:        "generate-jobs",
-			Aliases:     []string{"gj"},
-			Usage:       "generate-jobs <releaseurl>",
-			Description: "generate golang structs for the jobs in a given release",
+			Name:      "generate",
+			Usage:     "Generate golang structs for a given release",
+			ArgsUsage: "<releaseURL>",
+
 			Action: func(c *cli.Context) {
 				generators.GenerateReleaseJobsPackage(c.Args().First(), cacheDir, OutputDir)
 				println("completed generating release job structs for ", c.Args().First())
 			},
 		},
 		{
-			Name:    "diff-release",
-			Aliases: []string{"dr"},
-			Usage:   "show a diff between 2 releases given",
+			Name:      "diff",
+			Usage:     "Show changes between two releases",
+			ArgsUsage: "<release1URL> <release2URL>",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "job, j",
+					Usage: "Focus the command to a specific named `JOB`",
+				},
+			},
 			Action: func(c *cli.Context) {
-				d := run.NewDiffCmd(pull.Release{CacheDir: cacheDir}, c.Args()[0], c.Args()[1])
-				err := d.All(os.Stdout)
-				if err != nil {
-					fmt.Println(err.Error())
-					os.Exit(1)
+				d := run.NewDiffCmd(releaseRepo, c.Args()[0], c.Args()[1])
+				if len(c.String("job")) > 0 {
+					err = d.Job(c.String("job"), os.Stdout)
+				} else {
+					err = d.All(os.Stdout)
 				}
+				ifErrorDisplayAndExit(err)
 			},
 		},
 		{
-			Name:        "diff-job",
-			Aliases:     []string{"dj"},
-			Usage:       "diff-job <jobname> <releaseurl-A> <releaseurl-B>",
-			Description: "show diff between jobs across 2 releases",
+			Name:      "show",
+			Usage:     "Show details about the release",
+			ArgsUsage: "<releaseURL>",
 			Action: func(c *cli.Context) {
-				d := run.NewDiffCmd(pull.Release{CacheDir: cacheDir}, c.Args()[1], c.Args()[2])
-				err := d.Job(c.Args()[0], os.Stdout)
-				if err != nil {
-					fmt.Println(err.Error())
-					os.Exit(1)
-				}
-			},
-		},
-		{
-			Name:        "show",
-			Aliases:     []string{"sh"},
-			Usage:       "show <releaseurl>",
-			Description: "show all jobs and properties from the specified release",
-			Action: func(c *cli.Context) {
-				releaseRepo := pull.Release{CacheDir: cacheDir}
 				s := run.NewShowCmd(releaseRepo, c.Args()[0])
 				err := s.All(os.Stdout)
-				if err != nil {
-					fmt.Println(err.Error())
-					os.Exit(1)
-				}
+				ifErrorDisplayAndExit(err)
 			},
 		},
 	}
 	app.Run(os.Args)
+}
+
+func ifErrorDisplayAndExit(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
