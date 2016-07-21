a38225946bcef499473eb565359642f919376663
Zac Bergquist <zbergquist99@gmail.com>
2016-07-21 08:37:15 -0600

    [#126155341] clean up boshclient code to be a bit more idiomatic
    - return early on error
    - remove 'naked' returns
    
    This reduces the total lines of code and the indentation,
    making it easier to follow.

--- enamlbosh/api.go
+++ enamlbosh/api.go
@@ -23,210 +23,199 @@ func (s *Client) newRequest(method, url string, body io.Reader) (*http.Request, 
 	return req, err
 }
 
-func (s *Client) NewCloudConfigRequest(cloudconfig enaml.CloudConfigManifest) (req *http.Request, err error) {
-	var b []byte
-	var body io.Reader
-	if b, err = cloudconfig.Bytes(); err == nil {
-		body = bytes.NewReader(b)
-		req, err = s.newRequest("POST", s.buildBoshURL("/cloud_configs"), body)
-		req.Header.Set("content-type", "text/yaml")
+func (s *Client) NewCloudConfigRequest(cloudconfig enaml.CloudConfigManifest) (*http.Request, error) {
+	b, err := cloudconfig.Bytes()
+	if err != nil {
+		return nil, err
 	}
-	return
+	body := bytes.NewReader(b)
+	req, err := s.newRequest("POST", s.buildBoshURL("/cloud_configs"), body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("content-type", "text/yaml")
+	return req, nil
 }
 
-func (s *Client) GetTask(taskID int, httpClient HttpClientDoer) (bt BoshTask, err error) {
-	var req *http.Request
-	var res *http.Response
-
-	if req, err = s.newRequest("GET", s.buildBoshURL("/tasks/"+strconv.Itoa(taskID)), nil); err == nil {
-		req.Header.Set("content-type", "text/yaml")
-
-		if res, err = httpClient.Do(req); err == nil {
-			defer res.Body.Close()
-			lo.G.Debug("task request complete")
-			var b []byte
-			b, err = ioutil.ReadAll(res.Body)
-			lo.G.Debug("rest resp: ", string(b))
-			json.Unmarshal(b, &bt)
-		}
+func (s *Client) GetTask(taskID int, httpClient HttpClientDoer) (BoshTask, error) {
+	req, err := s.newRequest("GET", s.buildBoshURL("/tasks/"+strconv.Itoa(taskID)), nil)
+	if err != nil {
+		return BoshTask{}, err
 	}
-
-	if bt.ID != taskID && err == nil {
-		err = fmt.Errorf("could not find the given task: %v", taskID)
+	req.Header.Set("content-type", "text/yaml")
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return BoshTask{}, err
 	}
-	return
+	defer res.Body.Close()
+	lo.G.Debug("task request complete")
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return BoshTask{}, err
+	}
+	lo.G.Debug("rest resp: ", string(b))
+	var bt BoshTask
+	json.Unmarshal(b, &bt)
+
+	if bt.ID != taskID {
+		return bt, fmt.Errorf("could not find the given task: %v", taskID)
+	}
+	return bt, nil
 }
 
-func (s *Client) PostRemoteRelease(rls enaml.Release, httpClient HttpClientDoer) (bt BoshTask, err error) {
-
+func (s *Client) PostRemoteRelease(rls enaml.Release, httpClient HttpClientDoer) (BoshTask, error) {
 	if rls.URL == "" || rls.SHA1 == "" {
-		err = fmt.Errorf("url or sha not set. these are required for remote stemcells URL: %s , SHA: %s", rls.URL, rls.SHA1)
-
-	} else {
-		var req *http.Request
-		var res *http.Response
-		var reqMap = map[string]string{
-			"location": rls.URL,
-			"sha1":     rls.SHA1,
-		}
-		var reqBytes, _ = json.Marshal(reqMap)
-		var reqBody = bytes.NewReader(reqBytes)
-
-		if req, err = s.newRequest("POST", s.buildBoshURL("/releases"), reqBody); err == nil {
-			req.Header.Set("content-type", "application/json")
-
-			if res, err = httpClient.Do(req); err == nil {
-				defer res.Body.Close()
-				lo.G.Debug("release request complete")
-				var b []byte
-
-				if b, err = ioutil.ReadAll(res.Body); err == nil {
-					lo.G.Debug("rest resp: ", string(b))
-					err = json.Unmarshal(b, &bt)
-				}
-			}
-		}
+		return BoshTask{}, fmt.Errorf("url or sha not set. these are required for remote stemcells URL: %s , SHA: %s", rls.URL, rls.SHA1)
 	}
-	return
+	reqMap := map[string]string{
+		"location": rls.URL,
+		"sha1":     rls.SHA1,
+	}
+	reqBytes, _ := json.Marshal(reqMap)
+	reqBody := bytes.NewReader(reqBytes)
+
+	req, err := s.newRequest("POST", s.buildBoshURL("/releases"), reqBody)
+	if err != nil {
+		return BoshTask{}, err
+	}
+
+	req.Header.Set("content-type", "application/json")
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return BoshTask{}, err
+	}
+
+	defer res.Body.Close()
+	lo.G.Debug("release request complete")
+
+	var bt BoshTask
+	err = json.NewDecoder(res.Body).Decode(&bt)
+	return bt, err
 }
 
 func (s *Client) GetStemcells(httpClient HttpClientDoer) (stemcells []DeployedStemcell, err error) {
-	var req *http.Request
-	var res *http.Response
-
-	if req, err = http.NewRequest("GET", s.buildBoshURL("/stemcells"), nil); err == nil {
-		req.SetBasicAuth(s.user, s.pass)
-		req.Header.Set("content-type", "application/json")
-
-		if res, err = httpClient.Do(req); err == nil {
-			defer res.Body.Close()
-			var b []byte
-
-			if b, err = ioutil.ReadAll(res.Body); err == nil {
-				stemcells = make([]DeployedStemcell, 0)
-				err = json.Unmarshal(b, &stemcells)
-			}
-		}
+	req, err := s.newRequest("GET", s.buildBoshURL("/stemcells"), nil)
+	if err != nil {
+		return nil, err
 	}
+	req.Header.Set("content-type", "application/json")
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var stemcells []DeployedStemcell
+	err = json.NewDecoder(res.Body).Decode(&stemcells)
 	return stemcells, err
 }
 
 func (s *Client) CheckRemoteStemcell(sc enaml.Stemcell, httpClient HttpClientDoer) (exists bool, err error) {
-
 	if (sc.Name == "" && sc.OS == "") || sc.Version == "" {
-		err = fmt.Errorf("name or version not set. these are required to check for remote stemcells Name: %s , Version: %s", sc.Name, sc.Version)
+		return false, fmt.Errorf("name or version not set. these are required to check for remote stemcells Name: %s , Version: %s", sc.Name, sc.Version)
+	}
 
-	} else {
-		exists = false
+	stemcells, err := s.GetStemcells(httpClient)
+	if err != nil {
+		return false, err
+	}
 
-		if stemcells, err := s.GetStemcells(httpClient); err == nil {
-
-			for _, stemcell := range stemcells {
-
-				if (stemcell.Name == sc.Name || stemcell.OS == sc.OS) && stemcell.Version == sc.Version {
-					exists = true
-					break
-				}
-			}
+	for _, stemcell := range stemcells {
+		if (stemcell.Name == sc.Name || stemcell.OS == sc.OS) && stemcell.Version == sc.Version {
+			return true, nil
 		}
 	}
-	return
+	return false, nil
 }
 
-func (s *Client) PostRemoteStemcell(sc enaml.Stemcell, httpClient HttpClientDoer) (bt BoshTask, err error) {
-
+func (s *Client) PostRemoteStemcell(sc enaml.Stemcell, httpClient HttpClientDoer) (BoshTask, error) {
 	if sc.URL == "" || sc.SHA1 == "" {
-		err = fmt.Errorf("url or sha not set. these are required for remote stemcells URL: %s , SHA: %s", sc.URL, sc.SHA1)
-
-	} else {
-		var req *http.Request
-		var res *http.Response
-		var reqMap = map[string]string{
-			"location": sc.URL,
-			"sha1":     sc.SHA1,
-		}
-		var reqBytes, _ = json.Marshal(reqMap)
-		var reqBody = bytes.NewReader(reqBytes)
-
-		if req, err = s.newRequest("POST", s.buildBoshURL("/stemcells"), reqBody); err == nil {
-			req.Header.Set("content-type", "application/json")
-
-			if res, err = httpClient.Do(req); err == nil {
-				defer res.Body.Close()
-				lo.G.Debug("stemcell request complete")
-				var b []byte
-
-				if b, err = ioutil.ReadAll(res.Body); err == nil {
-					lo.G.Debug("rest resp: ", string(b))
-					err = json.Unmarshal(b, &bt)
-				}
-			}
-		}
+		return BoshTask{}, fmt.Errorf("url or sha not set. these are required for remote stemcells URL: %s , SHA: %s", sc.URL, sc.SHA1)
 	}
-	return
+	reqMap := map[string]string{
+		"location": sc.URL,
+		"sha1":     sc.SHA1,
+	}
+	reqBytes, _ := json.Marshal(reqMap)
+	reqBody := bytes.NewReader(reqBytes)
+
+	req, err := s.newRequest("POST", s.buildBoshURL("/stemcells"), reqBody)
+	if err != nil {
+		return BoshTask{}, err
+	}
+
+	req.Header.Set("content-type", "application/json")
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return BoshTask{}, err
+	}
+
+	defer res.Body.Close()
+	lo.G.Debug("stemcell request complete")
+
+	var bt BoshTask
+	err = json.NewDecoder(res.Body).Decode(&bt)
+	return bt, err
 }
 
-func (s *Client) PostDeployment(deploymentManifest enaml.DeploymentManifest, httpClient HttpClientDoer) (boshTask BoshTask, err error) {
-	var req *http.Request
-	var res *http.Response
-	var reqBody = bytes.NewReader(deploymentManifest.Bytes())
-
-	if req, err = s.newRequest("POST", s.buildBoshURL("/deployments"), reqBody); err == nil {
-		req.Header.Set("content-type", "text/yaml")
-
-		if res, err = httpClient.Do(req); err == nil {
-			defer res.Body.Close()
-			lo.G.Debug("deployment request complete")
-			var b []byte
-
-			if b, err = ioutil.ReadAll(res.Body); err == nil {
-				lo.G.Debug("rest resp: ", string(b))
-				err = json.Unmarshal(b, &boshTask)
-			}
-		}
+func (s *Client) PostDeployment(deploymentManifest enaml.DeploymentManifest, httpClient HttpClientDoer) (BoshTask, error) {
+	reqBody := bytes.NewReader(deploymentManifest.Bytes())
+	req, err := s.newRequest("POST", s.buildBoshURL("/deployments"), reqBody)
+	if err != nil {
+		return BoshTask{}, err
 	}
-	return
+
+	req.Header.Set("content-type", "text/yaml")
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return BoshTask{}, err
+	}
+
+	defer res.Body.Close()
+	lo.G.Debug("deployment request complete")
+	var bt BoshTask
+	err = json.NewDecoder(res.Body).Decode(&bt)
+	return bt, err
 }
 
-func (s *Client) GetCloudConfig(httpClient HttpClientDoer) (cloudconfig *enaml.CloudConfigManifest, err error) {
-	var req *http.Request
-	var res *http.Response
-	var resBody = make([]CloudConfigResponseBody, 1)
-
-	if req, err = s.newRequest("GET", s.buildBoshURL("/cloud_configs?limit=1"), nil); err == nil {
-		req.Header.Set("content-type", "text/yaml")
-
-		if res, err = httpClient.Do(req); err == nil {
-			defer res.Body.Close()
-			var b []byte
-			b, err = ioutil.ReadAll(res.Body)
-			lo.G.Debug("rest resp: ", string(b))
-			json.Unmarshal(b, &resBody)
-			cloudconfig = enaml.NewCloudConfigManifest([]byte(resBody[0].Properties))
-		}
+func (s *Client) GetCloudConfig(httpClient HttpClientDoer) (*enaml.CloudConfigManifest, error) {
+	req, err := s.newRequest("GET", s.buildBoshURL("/cloud_configs?limit=1"), nil)
+	if err != nil {
+		return nil, err
 	}
-	return
+	req.Header.Set("content-type", "text/yaml")
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var cc []CloudConfigResponseBody
+	err = json.NewDecoder(res.Body).Decode(&cc)
+	if err != nil {
+		return nil, err
+	}
+	return enaml.NewCloudConfigManifest([]byte(cc[0].Properties)), nil
 }
 
-func (s *Client) GetInfo(httpClient HttpClientDoer) (bi *BoshInfo, err error) {
-	var req *http.Request
-	var res *http.Response
-	bi = new(BoshInfo)
-
-	if req, err = s.newRequest("GET", s.buildBoshURL("/info"), nil); err == nil {
-		req.Header.Set("content-type", "text/yaml")
-
-		if res, err = httpClient.Do(req); err == nil {
-			var b []byte
-			b, err = ioutil.ReadAll(res.Body)
-			lo.G.Debug("rest resp: ", string(b))
-			json.Unmarshal(b, bi)
-		}
+func (s *Client) GetInfo(httpClient HttpClientDoer) (*BoshInfo, error) {
+	req, err := s.newRequest("GET", s.buildBoshURL("/info"), nil)
+	if err != nil {
+		return nil, err
 	}
-	return
+
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	var bi BoshInfo
+	err = json.NewDecoder(res.Body).Decode(&bi)
+	if err != nil {
+		return nil, err
+	}
+	return &bi, nil
 }
 
-func (s *Client) buildBoshURL(urlpath string) (boshurl string) {
-	boshurl = s.host + ":" + strconv.Itoa(s.port) + urlpath
-	return
+func (s *Client) buildBoshURL(urlpath string) string {
+	return s.host + ":" + strconv.Itoa(s.port) + urlpath
 }
